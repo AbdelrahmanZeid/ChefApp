13a7949cc9a61ccdb872f0a947487b5e1539a954
abdelrahman zeid <zeid4643@gmail.com>
2024-03-14 05:17:29 +0200

    create login model

---
+++ lib/features/auth/data/models/login_model.dart
@@ -0,0 +1,19 @@
+import 'package:chef_app/core/database/api/end_points.dart';
+import 'package:chef_app/features/auth/presentation/cubits/cubit/login_cubit.dart';
+
+class LoginModel {
+  final String message;
+  final String token;
+
+  LoginModel({
+    required this.message,
+    required this.token,
+  });
+
+  factory LoginModel.fromJson(Map<String, dynamic> jsonData) {
+    return LoginModel(
+      message: jsonData[ApiKeys.message],
+      token: jsonData[ApiKeys.token],
+    );
+  }
+}
