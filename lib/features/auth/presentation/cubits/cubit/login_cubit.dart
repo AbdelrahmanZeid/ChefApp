dfbdc2ee88a87da514678fe75296217721107ee4
abdelrahman zeid <zeid4643@gmail.com>
2024-03-14 04:50:06 +0200

    add login logic

--- lib/features/auth/presentation/cubits/cubit/login_cubit.dart
+++ lib/features/auth/presentation/cubits/cubit/login_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
-
-part 'login_state.dart';
+import 'package:chef_app/features/auth/presentation/cubits/cubit/login_state.dart';
+import 'package:flutter/material.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit()
+      : super(
+          LoginInitial(),
+        );
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  bool isVisiable = true;
+  void changeIcon() {
+    isVisiable = !isVisiable;
+    emit(
+      ChangeIconSuccessState(),
+    );
+  }
 }
--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,12 +1,16 @@
 import 'package:chef_app/core/utils/app_color.dart';
 import 'package:chef_app/core/utils/app_function.dart';
+import 'package:chef_app/core/utils/app_routes.dart';
 import 'package:chef_app/core/utils/app_string.dart';
 import 'package:chef_app/core/utils/app_text_style.dart';
 import 'package:chef_app/core/utils/assets.dart';
 import 'package:chef_app/core/widgets/custom_elevated_button.dart';
 import 'package:chef_app/core/widgets/custom_text_form_field.dart';
+import 'package:chef_app/features/auth/presentation/cubits/cubit/login_cubit.dart';
+import 'package:chef_app/features/auth/presentation/cubits/cubit/login_state.dart';
 import 'package:chef_app/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginView extends StatelessWidget {
@@ -17,18 +21,36 @@ class LoginView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Stack(
-          children: [
-            Image.asset(
-              width: double.infinity,
-              fit: BoxFit.cover,
-              Assets.assetsImagesBackgroundImage1,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16,
-              ),
-              child: Form(
+        child: BlocProvider(
+          create: (context) => LoginCubit(),
+          child: const LoginViewBody(),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Image.asset(
+          width: double.infinity,
+          fit: BoxFit.cover,
+          Assets.assetsImagesBackgroundImage1,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+          ),
+          child: BlocConsumer<LoginCubit, LoginState>(
+            listener: (context, state) {},
+            builder: (context, state) {
+              return Form(
+                key: context.read<LoginCubit>().formKey,
                 child: SingleChildScrollView(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -50,13 +72,13 @@ class LoginView extends StatelessWidget {
                       CustomTextFormField(
                         validate: (data) {
                           if (data!.length < 6 || data.isEmpty) {
-                            return AppStrings.pleaseEnterValidEmail;
+                            return S.of(context).please_enter_email;
                           } else {
                             return null;
                           }
                         },
                         hint: S.of(context).email,
-                        controller: TextEditingController(),
+                        controller: context.read<LoginCubit>().emailController,
                       ),
                       addSpace(
                         25,
@@ -64,16 +86,21 @@ class LoginView extends StatelessWidget {
                       CustomTextFormField(
                         validate: (data) {
                           if (data!.length < 6 || data.isEmpty) {
-                            return AppStrings.pleaseEnterValidPassword;
+                            return S.of(context).please_enter_valid_password;
                           } else {
                             return null;
                           }
                         },
-                        icon: Icons.visibility_off,
+                        icon: context.read<LoginCubit>().isVisiable
+                            ? Icons.visibility_off
+                            : Icons.visibility,
                         hint: S.of(context).password,
-                        isPassword: true,
-                        suffixIconOnPressed: () {},
-                        controller: TextEditingController(),
+                        isPassword: context.read<LoginCubit>().isVisiable,
+                        suffixIconOnPressed: () {
+                          context.read<LoginCubit>().changeIcon();
+                        },
+                        controller:
+                            context.read<LoginCubit>().passwordController,
                       ),
                       addSpace(
                         24,
@@ -93,6 +120,18 @@ class LoginView extends StatelessWidget {
                         height: 50.h,
                         bgColor: AppColors.primary,
                         title: S.of(context).sign_in,
+                        onPressed: () {
+                          if (context
+                              .read<LoginCubit>()
+                              .formKey
+                              .currentState!
+                              .validate()) {
+                            navigation(
+                              context,
+                              AppRoutes.profile,
+                            );
+                          }
+                        },
                       ),
                       addSpace(
                         30,
@@ -118,11 +157,11 @@ class LoginView extends StatelessWidget {
                     ],
                   ),
                 ),
-              ),
-            ),
-          ],
+              );
+            },
+          ),
         ),
-      ),
+      ],
     );
   }
 }
